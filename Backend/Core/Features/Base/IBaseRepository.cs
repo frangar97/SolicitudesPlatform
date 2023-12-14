4149fcc53e37d625760edda9a7e800c1d8bc0bf7
Francisco A. Garcia <frangarcia38@hotmail.com>
2023-12-14 08:51:54 -0600

    [feat]: Interfaz para realizar implementacion de repositorio generico

---
+++ Backend/Core/Features/Base/IBaseRepository.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq.Expressions;
+
+namespace Core.Features.Base
+{
+    public interface IBaseRepository<T> where T : BaseEntity
+    {
+        IEnumerable<T> GetAll();
+        Task AddAsync(T entity);
+        Task DeleteAsync(int id);
+        Task<T?> FindByIdAsync(int id);
+        Task<T?> FirstOrDefaultAsync(Expression<Func<T, bool>> expression);
+        IEnumerable<T> Where(Expression<Func<T, bool>> expression);
+        void Update(T entity);
+        IEnumerable<V> GetAllSelect<V>(Expression<Func<T, V>> expression);
+    }
+}
