41b45d8b40010730dc75980c7ded03c3dbf63881
Francisco A. Garcia <frangarcia38@hotmail.com>
2023-12-14 09:02:08 -0600

    [build]: Instalacion de dependencias para conectar con base de datos. [feat]: Se agrega la clase context para conectar con la base de datos.

---
+++ Backend/Persistence/AppContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Core.Features.EstadoSolicitud;
+using Core.Features.Genero;
+using Core.Features.TipoSolicitud;
+using Core.Features.TipoUsuario;
+using Core.Features.Zona;
+
+namespace Persistence
+{
+    public class AppContext:DbContext
+    {
+        public AppContext(DbContextOptions<AppContext> options):base(options)
+        {
+            
+        }
+
+        public DbSet<GeneroEntity> Genero { get; set; }
+        public DbSet<TipoSolicitudEntity> TipoSolicitud { get; set; }
+        public DbSet<TipoUsuarioEntity> TipoUsuario { get; set; }
+        public DbSet<EstadoSolicitudEntity> EstadoSolicitud { get; set; }
+        public DbSet<ZonaEntity> Zona { get; set; }
+    }
+}
