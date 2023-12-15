546d97a87a0dbf54d396af9b7b93f2c214713ebc
Francisco A. Garcia <frangarcia38@hotmail.com>
2023-12-14 20:29:24 -0600

    [feat]: servicio para encriptar y comparar contraseñas u otras encriptaciones

---
+++ Backend/Core/Services/IHashService.cs
@@ -0,0 +1,8 @@
+﻿namespace Core.Services
+{
+    public interface IHashService
+    {
+        string GenerateHash(string key);
+        bool CompareStringHash(string key, string value);
+    }
+}
---
+++ Backend/Infrastructure/Services/HashService.cs
@@ -0,0 +1,19 @@
+﻿using Core.Services;
+
+namespace Infrastructure.Services
+{
+    public class HashService : IHashService
+    {
+        public bool CompareStringHash(string key, string value)
+        {
+            bool isEqual = BCrypt.Net.BCrypt.Verify(key, value);
+            return isEqual;
+        }
+
+        public string GenerateHash(string key)
+        {
+            string hash = BCrypt.Net.BCrypt.HashPassword(key);
+            return hash;
+        }
+    }
+}
