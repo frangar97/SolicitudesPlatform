d8122f1ee246316967d82f89177cc003255cb108
Francisco A. Garcia <frangarcia38@hotmail.com>
2023-12-15 14:33:20 -0600

    [feat]: creacion de enums para manejar estados

---
+++ Backend/Core/Features/EstadoSolicitud/Enums/EstadoSolicitudEnum.cs
@@ -0,0 +1,9 @@
+﻿namespace Core.Features.EstadoSolicitud.Enums
+{
+    public enum EstadoSolicitudEnum
+    {
+        Pendiente = 1,
+        Aprobado = 2,
+        Cancelado = 3
+    }
+}
---
+++ Backend/Core/Features/Genero/Enums/GeneroEnum.cs
@@ -0,0 +1,8 @@
+﻿namespace Core.Features.Genero.Enums
+{
+    public enum GeneroEnum
+    {
+        Masculino = 1,
+        Femenino = 2
+    }
+}
