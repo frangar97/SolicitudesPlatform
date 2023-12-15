95aeed8275ddba9a1424a6dcfda743e104b84ac5
Francisco A. Garcia <frangarcia38@hotmail.com>
2023-12-15 14:14:00 -0600

    [feat]: controlador auth y migraciones de tablas pendientes

---
+++ Backend/API/Controllers/AuthController.cs
@@ -0,0 +1,24 @@
+﻿using Core.Features.Usuario.DTO;
+using Core.Features.Usuario.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IUsuarioService _usuarioService;
+        public AuthController(IUsuarioService usuarioService)
+        {
+            _usuarioService = usuarioService;
+        }
+
+        [HttpPost("registrar")]
+        public async Task<IActionResult> RegisterUsuario([FromBody]CreateUsuarioDTO createUsuarioDTO)
+        {
+            UsuarioDTO usuario = await _usuarioService.CreateUsuario(createUsuarioDTO);
+            return Ok(usuario);
+        }
+    }
+}
---
+++ Backend/Persistence/Migrations/20231215200728_configuracion-relaciones.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class configuracionrelaciones : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Usuario",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Apellido = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Codigo = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ImagenUrl = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    GeneroId = table.Column<int>(type: "int", nullable: false),
+                    TipoUsuarioId = table.Column<int>(type: "int", nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValueSql: "GETDATE()"),
+                    ModifiedDate = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValueSql: "GETDATE()")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Usuario", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Usuario_Genero_GeneroId",
+                        column: x => x.GeneroId,
+                        principalTable: "Genero",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Usuario_TipoUsuario_TipoUsuarioId",
+                        column: x => x.TipoUsuarioId,
+                        principalTable: "TipoUsuario",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Solicitud",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Descripcion = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    UsuarioId = table.Column<int>(type: "int", nullable: false),
+                    EstadoSolicitudId = table.Column<int>(type: "int", nullable: false),
+                    TipoSolicitudId = table.Column<int>(type: "int", nullable: false),
+                    ZonaId = table.Column<int>(type: "int", nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValueSql: "GETDATE()"),
+                    ModifiedDate = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValueSql: "GETDATE()")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Solicitud", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Solicitud_EstadoSolicitud_EstadoSolicitudId",
+                        column: x => x.EstadoSolicitudId,
+                        principalTable: "EstadoSolicitud",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Solicitud_TipoSolicitud_TipoSolicitudId",
+                        column: x => x.TipoSolicitudId,
+                        principalTable: "TipoSolicitud",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Solicitud_Usuario_UsuarioId",
+                        column: x => x.UsuarioId,
+                        principalTable: "Usuario",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Solicitud_Zona_ZonaId",
+                        column: x => x.ZonaId,
+                        principalTable: "Zona",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UsuarioSolicitud",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Activo = table.Column<bool>(type: "bit", nullable: false),
+                    UsuarioId = table.Column<int>(type: "int", nullable: false),
+                    TipoSolicitudId = table.Column<int>(type: "int", nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValueSql: "GETDATE()"),
+                    ModifiedDate = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValueSql: "GETDATE()")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UsuarioSolicitud", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_UsuarioSolicitud_TipoSolicitud_TipoSolicitudId",
+                        column: x => x.TipoSolicitudId,
+                        principalTable: "TipoSolicitud",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_UsuarioSolicitud_Usuario_UsuarioId",
+                        column: x => x.UsuarioId,
+                        principalTable: "Usuario",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UsuarioZona",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Activo = table.Column<bool>(type: "bit", nullable: false),
+                    UsuarioId = table.Column<int>(type: "int", nullable: false),
+                    ZonaId = table.Column<int>(type: "int", nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValueSql: "GETDATE()"),
+                    ModifiedDate = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValueSql: "GETDATE()")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UsuarioZona", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_UsuarioZona_Usuario_UsuarioId",
+                        column: x => x.UsuarioId,
+                        principalTable: "Usuario",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_UsuarioZona_Zona_ZonaId",
+                        column: x => x.ZonaId,
+                        principalTable: "Zona",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Solicitud_EstadoSolicitudId",
+                table: "Solicitud",
+                column: "EstadoSolicitudId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Solicitud_TipoSolicitudId",
+                table: "Solicitud",
+                column: "TipoSolicitudId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Solicitud_UsuarioId",
+                table: "Solicitud",
+                column: "UsuarioId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Solicitud_ZonaId",
+                table: "Solicitud",
+                column: "ZonaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Usuario_GeneroId",
+                table: "Usuario",
+                column: "GeneroId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Usuario_TipoUsuarioId",
+                table: "Usuario",
+                column: "TipoUsuarioId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UsuarioSolicitud_TipoSolicitudId",
+                table: "UsuarioSolicitud",
+                column: "TipoSolicitudId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UsuarioSolicitud_UsuarioId",
+                table: "UsuarioSolicitud",
+                column: "UsuarioId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UsuarioZona_UsuarioId",
+                table: "UsuarioZona",
+                column: "UsuarioId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UsuarioZona_ZonaId",
+                table: "UsuarioZona",
+                column: "ZonaId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Solicitud");
+
+            migrationBuilder.DropTable(
+                name: "UsuarioSolicitud");
+
+            migrationBuilder.DropTable(
+                name: "UsuarioZona");
+
+            migrationBuilder.DropTable(
+                name: "Usuario");
+        }
+    }
+}
