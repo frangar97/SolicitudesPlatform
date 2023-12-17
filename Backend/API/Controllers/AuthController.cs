b6b4093463333088047c13236711d37666527a2c
Francisco A. Garcia <frangarcia38@hotmail.com>
2023-12-17 10:51:19 -0600

    [feat]: obtencion de tipos de usuario

--- Backend/API/Controllers/AuthController.cs
+++ Backend/API/Controllers/AuthController.cs
@@ -1,4 +1,6 @@
-﻿using Core.Features.Usuario.DTO;
+﻿using Core.Features.TipoUsuario.DTO;
+using Core.Features.TipoUsuario.Services;
+using Core.Features.Usuario.DTO;
 using Core.Features.Usuario.Services;
 using Infrastructure.MediaUpload;
 using Microsoft.AspNetCore.Mvc;
@@ -10,10 +12,12 @@ namespace API.Controllers
     public class AuthController : ControllerBase
     {
         private readonly IUsuarioService _usuarioService;
+        private readonly ITipoUsuarioService _tipoUsuarioService;
         private readonly IMediaUpload _mediaUpload;
-        public AuthController(IUsuarioService usuarioService, IMediaUpload mediaUpload)
+        public AuthController(IUsuarioService usuarioService, IMediaUpload mediaUpload,ITipoUsuarioService tipoUsuarioService)
         {
             _usuarioService = usuarioService;
+            _tipoUsuarioService = tipoUsuarioService;
             _mediaUpload = mediaUpload;
         }
 
@@ -37,6 +41,13 @@ namespace API.Controllers
         {
             string token = await _usuarioService.LoginUsuarioAsync(loginUsuarioDTO);
             return Ok(new { token = token });
+        }
+
+        [HttpGet("tipousuario")]
+        public IActionResult ObtenerTiposDeUsuario()
+        {
+            IEnumerable<TipoUsuarioDTO> tiposUsuario = _tipoUsuarioService.GetAllTiposUsuario();
+            return Ok(tiposUsuario);
         }
     }
 }
